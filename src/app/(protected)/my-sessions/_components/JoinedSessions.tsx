73919759e75cbe7c131c26d5b7843fea11301c30
oyunmyagmar <oyunmyagmar.g@gmail.com>
2025-11-25 17:50:58 +0800

    added

--- src/app/(protected)/my-sessions/_components/JoinedSessions.tsx
+++ src/app/(protected)/my-sessions/_components/JoinedSessions.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { SessionCard } from "./SessionCard";
 
 const joinedMock = [
--- src/app/(protected)/my-sessions/page.tsx
+++ src/app/(protected)/my-sessions/page.tsx
@@ -1,13 +1,13 @@
-import { JoinedSessions } from "./_components/JoinedSessions";
-import { MoreSessions } from "./_components/MoreSessions";
-import { SessionDetails } from "./_components/SessionDetails";
+import React from "react";
+
+import { JoinedSessions, MoreSessions, SessionDetails } from "./_components";
 
 const MySessionPage = () => {
   return (
-    <div className="w-full min-h-screen flex gap-8 px-10 py-10">
+    <div className="w-full min-h-screen flex gap-8 p-10">
       {/* Left side session lists */}
       <div className="flex-1">
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-20">
           <JoinedSessions />
           <MoreSessions />
         </div>
