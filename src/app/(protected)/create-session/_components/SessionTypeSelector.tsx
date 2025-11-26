3692bdc865ed32a65e52302aa4f8868b5dd804fb
oyunmyagmar <25LP4018@nest.edu.mn>
2025-11-27 05:20:24 +0900

    added

--- src/app/(protected)/create-session/_components/SessionTypeSelector.tsx
+++ src/app/(protected)/create-session/_components/SessionTypeSelector.tsx
@@ -40,17 +40,14 @@ export const SessionTypeSelector = ({
     setTutorLedInputValue("");
   };
 
-  const deleteSelectedTutor = (tutor: string) => {
-    console.log({ tutor });
-    console.log(selectedTutors, "aaaaaaa");
-    const remainSelectedTutors = selectedTutors.filter(
+  const deleteSelectedTutor = (tutorEmail: string) => {
+    const remainedSelectedTutors = selectedTutors.filter(
       (selectedTutor: { mockTutorEmail: string }) =>
-        selectedTutor.mockTutorEmail !== tutor
+        selectedTutor.mockTutorEmail !== tutorEmail
     );
-    if (remainSelectedTutors) {
-      setSelectedTutors(remainSelectedTutors);
+    if (remainedSelectedTutors) {
+      setSelectedTutors(remainedSelectedTutors);
     }
-    console.log({ remainSelectedTutors });
   };
 
   return (
@@ -76,35 +73,43 @@ export const SessionTypeSelector = ({
       </RadioGroup>
 
       {selectedSessionType === "tutor-led" && (
-        <div className="flex justify-between gap-3">
-          <Input
-            list="tutors"
-            placeholder="Type your tutors here..."
-            className="border bg-none px-3 py-2 text-sm border-[#323743FF] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed
+        <div className=" flex flex-col gap-0.5">
+          <div className="flex justify-between gap-3">
+            <Input
+              list="tutors"
+              placeholder="Type your tutors here..."
+              className="border bg-none px-3 py-2 text-sm border-[#323743FF] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed
           disabled:opacity-50"
-            value={tutorLedInputValue}
-            onChange={(e) => setTutorLedInputValue(e.target.value)}
-          />
-          <datalist id="tutors">
-            {mockTutors.map((mockTutor) => (
-              <option key={mockTutor._id} value={mockTutor.mockTutorEmail} />
-            ))}
-          </datalist>
-          <Button
-            variant={"outline"}
-            className="bg-transparent border-[#323743FF] hover:bg-[#FFFFFF14] hover:text-primary-foreground"
-            onClick={addSelectedTutors}
-            disabled={selectedTutors.length > 2}
-          >
-            Add
-          </Button>
+              value={tutorLedInputValue}
+              onChange={(e) => setTutorLedInputValue(e.target.value)}
+            />
+
+            <datalist id="tutors">
+              {mockTutors.map((mockTutor) => (
+                <option key={mockTutor._id} value={mockTutor.mockTutorEmail} />
+              ))}
+            </datalist>
+            <Button
+              variant={"outline"}
+              className="bg-transparent border-[#323743FF] hover:bg-[#FFFFFF14] hover:text-primary-foreground"
+              onClick={addSelectedTutors}
+              disabled={selectedTutors.length > 2}
+            >
+              Add
+            </Button>
+          </div>
+          {selectedTutors.length > 2 && (
+            <div className="text-xs text-orange-500">
+              * You can select up to 3 tutors only
+            </div>
+          )}
         </div>
       )}
       {selectedTutors && (
         <div>
           {selectedTutors.map((tutor, index) => {
             return (
-              <div key={index} className="flex justify-between">
+              <div key={index} className="flex justify-between items-center">
                 <div>{tutor.mockTutorEmail}</div>
 
                 <Button
