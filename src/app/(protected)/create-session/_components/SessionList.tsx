f04474096e4e58196cb66878d7979c262169d8ab
Khishigdari <nbhishgee22@gmail.com>
2025-12-01 15:52:22 +0800

    session list skeleton 12.1

--- src/app/(protected)/create-session/_components/SessionList.tsx
+++ src/app/(protected)/create-session/_components/SessionList.tsx
@@ -1,24 +1,22 @@
 "use cLient";
 
-import React, { Dispatch } from "react";
+import React, { Dispatch, useState } from "react";
 import { Button } from "@/components/ui";
 import { BsFillPeopleFill, BsLink } from "react-icons/bs";
 import { useSession } from "@/app/_hooks/use-session";
 import { toast } from "sonner";
 import { SessionInfoDialog } from "@/app/(protected)/create-session/_components";
 import { useRouter } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const SessionList = ({
   userId,
-  maxMember,
-  setMaxMember,
 }: {
   userId: string;
   maxMember: number;
   setMaxMember: Dispatch<React.SetStateAction<number>>;
 }) => {
-  const { allSessions } = useSession();
-  const allllJoinedStudent = 5;
+  const { allSessions, isLoading } = useSession();
   const router = useRouter();
 
   const joinedStudentHandler = async (sessionId: string) => {
@@ -46,37 +44,50 @@ export const SessionList = ({
     <div className="flex flex-col gap-3">
       <h2 className="text-2xl leading-7 font-semibold">Sessions</h2>
 
-      <div className="flex flex-col gap-3">
-        {allSessions.map((session) => (
-          <div
-            key={session._id}
-            className="flex justify-between items-center p-4 rounded-3xl bg-[linear-gradient(180deg,#1E2648_0%,#122136_100%)] hover:bg-white/10"
-          >
-            <SessionInfoDialog session={session} />
-
-            <div className="flex flex-col justify-center gap-2">
-              <Button
-                disabled={allSessions.length === 10}
-                onClick={() => {
-                  joinedStudentHandler(session._id);
-                }}
-                className="rounded-full bg-[#2563EB] hover:bg-[#1d4ed8] gap-1 cursor-pointer text-white/80 hover:text-white"
-              >
-                <BsFillPeopleFill />
-                <div>
-                  <span>{allSessions.length}</span>
-                  <span>/{session.maxMember}</span>
-                </div>
-                <div>JOIN</div>
-              </Button>
-
-              <Button className="rounded-full bg-[#2563EB17] hover:bg-[#2563EB33] gap-1 font-bold text-[#1d4ed8] hover:text-[#2563EB] cursor-pointer">
-                <BsLink />
-                <div>Invite</div>
-              </Button>
-            </div>
+      <div>
+        {isLoading ? (
+          <div className="flex flex-col gap-3">
+            {[1, 2, 3].map((i) => (
+              <Skeleton
+                key={i}
+                className="max-w-138 h-28 rounded-3xl opacity-10"
+              />
+            ))}
           </div>
-        ))}
+        ) : (
+          <div className="flex flex-col gap-3">
+            {allSessions.map((session) => (
+              <div
+                key={session._id}
+                className="flex justify-between items-center p-4 rounded-3xl bg-[linear-gradient(180deg,#1E2648_0%,#122136_100%)] hover:bg-white/10"
+              >
+                <SessionInfoDialog session={session} />
+
+                <div className="flex flex-col justify-center gap-2">
+                  <Button
+                    disabled={allSessions.length === 10}
+                    onClick={() => {
+                      joinedStudentHandler(session._id);
+                    }}
+                    className="rounded-full bg-[#2563EB] hover:bg-[#1d4ed8] gap-1 cursor-pointer text-white/80 hover:text-white"
+                  >
+                    <BsFillPeopleFill />
+                    <div>
+                      <span>{allSessions.length}</span>
+                      <span>/{session.maxMember}</span>
+                    </div>
+                    <div>JOIN</div>
+                  </Button>
+
+                  <Button className="rounded-full bg-[#2563EB17] hover:bg-[#2563EB33] gap-1 font-bold text-[#1d4ed8] hover:text-[#2563EB] cursor-pointer">
+                    <BsLink />
+                    <div>Invite</div>
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
