ed0fa24f2c05be13fce30bdc7f33dc7a88f811f9
Khishigdari <nbhishgee22@gmail.com>
2025-12-01 09:14:41 +0800

    img tag

--- src/app/_components-main-page/TierCard.tsx
+++ src/app/_components-main-page/TierCard.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Image from "next/image";
-
 interface TierCardProps {
   image: string;
   tier: number;
@@ -11,7 +9,7 @@ interface TierCardProps {
 export const TierCard = ({ image, tier, locked = false }: TierCardProps) => {
   return (
     <div className="relative w-28 h-28 flex justify-center items-center border border-[#2563EB99] rounded-xl bg-[#0e2644]">
-      <Image
+      <img
         src={image}
         alt="Tier reward"
         width={80}
