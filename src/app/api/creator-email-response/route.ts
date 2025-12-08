449b86cf9d00dd269b2fee4b740c543489fda243
oyunmyagmar <oyunmyagmar.g@gmail.com>
2025-12-08 19:35:36 +0800

    added

--- src/app/api/creator-email-response/route.ts
+++ src/app/api/creator-email-response/route.ts
@@ -1,3 +1,5 @@
+import { transporter } from "@/lib/mailer";
+import { MockUser } from "@/lib/models/MockUser";
 import { Session } from "@/lib/models/Session";
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
@@ -17,6 +19,12 @@ export async function GET(request: NextRequest) {
   }
 
   const session = await Session.findById(sessionId);
+  const students = await MockUser.find(
+    {
+      _id: { $in: session.studentCount },
+    },
+    "mockerUserEmail"
+  );
 
   if (!session) {
     return NextResponse.json(
@@ -28,8 +36,22 @@ export async function GET(request: NextRequest) {
   if (action === "cancel") {
     session.status = "CANCELED";
     await session.save();
-    //notify students
-    //send cancellation mails
+
+    await Promise.all(
+      students.map((student) =>
+        transporter.sendMail({
+          from: "Study Buddy <oyunmyagmar.g@gmail.com>",
+          to: student.mockUserEmail,
+          subject: "Session Cancellation",
+          html: `
+          <p>Your session has been cancelled.</p>`,
+        })
+      )
+    );
+
+    return NextResponse.redirect(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/creator/canceled`
+    );
   }
 
   if (action === "self") {
@@ -37,10 +59,20 @@ export async function GET(request: NextRequest) {
     session.status = "ACCEPTED";
     await session.save();
 
-    //notify students
+    await Promise.all(
+      students.map((student) =>
+        transporter.sendMail({
+          from: "Study Buddy <oyunmyagmar.g@gmail.com>",
+          to: student.mockerUserEmail,
+          subject: "Session Type Changed",
+          html: `
+          <p>Your session type has been changed to SELF-LED</p>`,
+        })
+      )
+    );
   }
 
   return NextResponse.redirect(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/creator/done`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/creator/self-led`
   );
 }
